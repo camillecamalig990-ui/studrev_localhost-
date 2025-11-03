54ad8340cecf3da56552ff0d4ce08a1972b306ce
Camille Antoinette Camalig <camillecamalig990@gmail.com>
2025-11-03 16:31:50 +0800

    Serve public folder correctly

--- server.js
+++ server.js
@@ -1,15 +1,9 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
 const app = express();
 
-// Serve all static files in 'public' folder
-app.use(express.static(path.join(__dirname, "public")));
+// Serve files from your public folder
+app.use(express.static(path.join(__dirname, 'stud.rev-localhost/public')));
 
-// Serve index.html on root
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
-
-// Use the port that Render assigns or 3000 locally
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
